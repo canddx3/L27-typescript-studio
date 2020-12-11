038e14ad8dc262a4de45178c374642f61a05f260
chad <canddx3@gmail.com>
2020-12-10 20:40:36 -0600

    first commit

---
+++ index.ts
@@ -0,0 +1,49 @@
+// Instructions are published in the online book. The URL is:
+// https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/studio.html
+
+// TODO: 
+// * Code the Astronaut, Cargo, and Rocket classes in new files.
+// * Import the three classes into this file.
+import { Rocket } from './Rocket';
+import { Astronaut } from './Astronaut';
+import { Cargo } from './Cargo';
+
+let falcon9: Rocket = new Rocket('Falcon 9', 7500);
+
+let astronauts: Astronaut[] = [
+    new Astronaut(75, 'Mae'),
+    new Astronaut(81, 'Sally'),
+    new Astronaut(99, 'Charles')
+];
+
+for (let i = 0; i < Astronaut.length; i++) {
+    let astronaut = astronauts[i];
+    let status = '';
+    if (falcon9.addAstronaut(astronaut)) {
+        status = 'On board';
+    } else {
+        status = 'Not on board';
+    }
+    console.log(`${astronaut.name}: ${status}`);
+}
+
+let cargo: Cargo[] = [
+    new Cargo(3107.39, 'Satellite'),
+    new Cargo(1000.39, 'Space Probe'),
+    new Cargo(783, 'Water'),
+    new Cargo(541, 'Food'),
+    new Cargo(2107.39, 'Tesla Roadster'),
+];
+
+for (let i = 0; i < cargo.length; i++) {
+    let c = cargo[i];
+    let loaded = '';
+    if (falcon9.addCargo(c)) {
+        loaded = 'loaded'
+    } else {
+        loaded = 'Not loaded'
+    }
+    console.log(`${c.material}: ${loaded}`);
+}
+
+console.log(`Final Cargo and astronaut mass: ${falcon9.currentMassKg()} kg.`);
